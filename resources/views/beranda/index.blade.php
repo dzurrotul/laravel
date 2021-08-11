e11946bd180d4af6054d415bbc6a0d538aa8eb9a
dzurrotul <dzurrokw@gmail.com>
2021-08-11 23:50:06 +0700

    laravel

---
+++ resources/views/beranda/index.blade.php
@@ -0,0 +1,27 @@
+@extends('layout')
+
+@section('content')
+    <header>
+
+
+<!-- Background image -->
+<div
+    class="p-5 text-center bg-image"
+    style="
+    background-image: url('https://mdbcdn.b-cdn.net/img/new/slides/041.jpg');
+    height:560px;
+    "
+>
+<div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+    <div class="d-flex justify-content-center align-items-center h-100">
+        <div class="text-white">
+            <h1 class="mb-3">Welcome</h1>
+
+
+        </div>
+    </div>
+</div>
+</div>
+<!-- Background image -->
+    </header>
+@endsection
---
+++ resources/views/profil/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layout')
+
+@section('content')
+    <header>
+
+
+<!-- Background image -->
+<div
+    class="p-5 text-center bg-image"
+    style="
+    background-image: url('https://mdbcdn.b-cdn.net/img/new/slides/041.jpg');
+    height:560px;
+    "
+>
+<div class="mask" style="background-color: rgba(0, 0, 0, 0.6);">
+    <div class="d-flex justify-content-center align-items-center h-100">
+        <div class="text-white">
+            <h1 class="mb-3">Profil Saya</h1>
+            <h4 class="mb-3">Nama : Dzurrotul A'immah</h4>
+            <h4 class="mb-3">kelas : XII RPL 1</h4>
+            <h4 class="mb-3">SMKN 1 PURWOSARI</h4>
+
+
+        </div>
+    </div>
+</div>
+</div>
+<!-- Background image -->
+    </header>
+@endsection
